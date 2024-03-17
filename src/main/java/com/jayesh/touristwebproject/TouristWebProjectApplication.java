a857596624979bc8d67ccdb3d4d5a1a2ca577d95
Jayesh445 <jayesh040405@gmail.com>
2024-03-17 23:30:17 +0530

    first commit

---
+++ src/main/java/com/jayesh/touristwebproject/TouristWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jayesh.touristwebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristWebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayesh/touristwebproject/TouristWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayesh.touristwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
